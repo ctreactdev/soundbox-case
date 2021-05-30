99fb536676aed2d1124c6685e5f861c8c846b03a
ctreactdev <83812404+ctreactdev@users.noreply.github.com>
2021-05-30 21:51:02 +0200

    feat(soundbox): readme + alt on img

--- src/_components/organisms/Header/index.tsx
+++ src/_components/organisms/Header/index.tsx
@@ -7,8 +7,8 @@ const Header: React.FunctionComponent<IHeaderProps> = ({
 }) => {
     return (
         <div className={styles.header}>
-            <a href="/" className={styles.headerLogo}>Soundbox logo</a>
-            {/* <img src="../../../../public/img/logo.svg" alt="" width={22} height={22}/> */}
+            <a href="/" className={styles.headerLogo}>SOUNDBOKS</a>
+            {/* <img src="../../../../public/img/logo.svg" alt="Soundboks logo" width={22} height={22}/> */}
             <div className={styles.headerDivided}>
                 <a href="/products">Products</a>
                 <a href="/ourBlog">Our Blog</a>
