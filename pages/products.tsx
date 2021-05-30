b379a036373e86b8aa62da915c77d76738c1ed7d
ctreactdev <83812404+ctreactdev@users.noreply.github.com>
2021-05-30 15:13:05 +0200

    feat(soundbox): atomic setup + card

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,9 +1,14 @@
 import styles from '../styles/Home.module.css'
+import { Card } from '../src/_components/atoms'
 
 export default function Products() {
   return (
     <div className={styles.container}>
-      <h1>products</h1>
+
+      <h1 >products</h1>
+      <Card
+        title={"box2"}
+      />
     </div>
   )
 }
